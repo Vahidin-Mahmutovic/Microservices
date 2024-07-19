a753e8b663e6b951d93c7203518e5f764d761e4c
Vahidin <mahmutovic.vahidin@hotmail.com>
2024-07-19 16:39:35 +0200

    Microservices

---
+++ service-registry/src/test/java/com/vahidin/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vahidin.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
